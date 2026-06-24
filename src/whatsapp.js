146269bf72573554964451467760226e580cbd7d
iopopa <ioana.popa@inf.ethz.ch>
2026-06-23 21:04:42 -0400

    vssie_event_registration

--- src/whatsapp.js
+++ src/whatsapp.js
@@ -39,7 +39,7 @@ export function sendOTP(phone, code) {
     to: phone,
     type: 'template',
     template: {
-      name: 'event_registration',
+      name: 'vssie_event_registration',
       language: { code: 'en_US' },
     },
   });
